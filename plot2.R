ef3c72707c03d36f38dd889b4cb084e06a722e04
Danielle Reed <reed@monell.org>
2017-01-03 08:27:41 -0500

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,39 @@
+# Homework for Week 1 of Exploratory Data for Coursera
+
+#The dataset has 2,075,259 rows and 9 columns. 
+#calculate memory
+GBmemory <- (2 * ((2075259 * 9 * 8)/2^20))/1000
+#about 0.50 GB including "overhead" - no problems
+
+#makes a folder called 'data' if none exists
+if (!file.exists("data")){
+  dir.create("data")
+}
+
+#download and upzip
+setwd("C:/Users/Reed/Dropbox/Dani life/2016 Coursera/Course 4 - Exploratory Data/data")
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, destfile = "data.zip")
+list.files("./")
+unzip(zipfile="./data.zip", exdir="./expanded")
+
+setwd("C:/Users/Reed/Dropbox/Dani life/2016 Coursera/Course 4 - Exploratory Data/data/expanded")
+#this code to read the table is from:
+#https://rstudio-pubs-static.s3.amazonaws.com/39904_05628d89d4a345b8a41f37d3bcabf958.html
+#However, I added the na.strings command so that ? read as NA (I hope)
+fh <- file("household_power_consumption.txt")
+#Limits the import of data to the two months we need
+ba <- read.table(text = grep("^[1,2]/2/2007", readLines(fh), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", na.strings = "?", header = TRUE)
+#chucks out incomplete cases
+ba <- ba[complete.cases(ba),]
+#convert to proper date, concatenate and turns into POSIXct for graphing
+ba$Date <- as.Date(ba$Date, "%d/%m/%Y")
+ba$dateTime <- paste(ba$Date, ba$Time)
+ba$dateTime <- as.POSIXct(ba$dateTime)
+
+#Plot 2
+plot(ba$Global_active_power~ba$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
+
+
---
+++ plot4.R
@@ -0,0 +1,61 @@
+# Homework for Week 1 of Exploratory Data for Coursera
+
+#The dataset has 2,075,259 rows and 9 columns. 
+#calculate memory
+GBmemory <- (2 * ((2075259 * 9 * 8)/2^20))/1000
+#about 0.50 GB including "overhead" - no problems
+
+#makes a folder called 'data' if none exists
+if (!file.exists("data")){
+  dir.create("data")
+}
+
+#download and upzip
+setwd("C:/Users/Reed/Dropbox/Dani life/2016 Coursera/Course 4 - Exploratory Data/data")
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, destfile = "data.zip")
+list.files("./")
+unzip(zipfile="./data.zip", exdir="./expanded")
+
+setwd("C:/Users/Reed/Dropbox/Dani life/2016 Coursera/Course 4 - Exploratory Data/data/expanded")
+#this code to read the table is from:
+#https://rstudio-pubs-static.s3.amazonaws.com/39904_05628d89d4a345b8a41f37d3bcabf958.html
+#However, I added the na.strings command so that ? read as NA (I hope)
+fh <- file("household_power_consumption.txt")
+#Limits the import of data to the two months we need
+ba <- read.table(text = grep("^[1,2]/2/2007", readLines(fh), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", na.strings = "?", header = TRUE)
+#chucks out incomplete cases
+ba <- ba[complete.cases(ba),]
+#convert to proper date, concatenate and turns into POSIXct for graphing
+ba$Date <- as.Date(ba$Date, "%d/%m/%Y")
+ba$dateTime <- paste(ba$Date, ba$Time)
+ba$dateTime <- as.POSIXct(ba$dateTime)
+
+#Plot 4
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(ba, {
+  plot(Global_active_power~dateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~dateTime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~dateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~dateTime,col='Red')
+  lines(Sub_metering_3~dateTime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n", cex = 0.4,
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~dateTime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
+dev.copy(png,"plot4.png", width=480, height=480)
+dev.off()
+
+
+
+
+
+
+
+
+
+
